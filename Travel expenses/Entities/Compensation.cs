a4498c61ae8bf7e6b04d864135a6dbad0f0ee482
Nurteginn <125720888+Nurteginn@users.noreply.github.com>
2023-03-18 16:49:13 +0600

    Update Compensation.cs

--- Travel expenses/Entities/Compensation.cs
+++ Travel expenses/Entities/Compensation.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public Employee Employee { get; set; }
+        public int EmployeeId { get; set; }
         public Trips Trip { get; set; }
+        public int TripId { get; set; }
         public int Total { get; set; }
     }
 }
